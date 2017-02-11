50158153f19747b9737d67e6b9b6d13952409c3f
daxapps <daxapps777@gmail.com>
2017-02-11 08:16:35 -0600

    Setup custom collectionCell with xib

---
+++ MyTacoPOP/TacoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TacoCell.swift
+//  MyTacoPOP
+//
+//  Created by Jason Crawford on 2/11/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+
+class TacoCell: UICollectionViewCell {
+
+    @IBOutlet weak var tacoImage: UIImageView!
+    @IBOutlet weak var tacoLabel: UILabel!
+    
+    var taco: Taco!
+    
+    func configureCell(taco: Taco) {
+        self.taco = taco
+        tacoImage.image = UIImage(named: taco.proteinId.rawValue)
+        tacoLabel.text = taco.productName
+    }
+
+}
